fe7f5b356e7d99d34bbe02cc41fe7e5c38555f07
leejay210 <leejay@ponylink.co.kr>
2025-04-08 12:26:39 +0900

    create imagePaste extension

---
+++ client/src/components/editor/extenstions/imagePaste.js
@@ -0,0 +1,39 @@
+import { Extension } from '@tiptap/core'
+import { Plugin } from 'prosemirror-state'
+
+const ImagePaste = Extension.create({
+  name: 'imagePaste',
+
+  addProseMirrorPlugins() {
+    return [
+      new Plugin({
+        props: {
+          handlePaste: (view, event) => {
+            const items = event.clipboardData?.items
+            if (!items) return false
+
+            for (const item of items) {
+              if (item.type.indexOf('image') === 0) {
+                const file = item.getAsFile()
+                const reader = new FileReader()
+                reader.onload = () => {
+                  const node = this.editor.schema.nodes.image.create({
+                    src: reader.result,
+                  })
+                  const transaction = this.editor.state.tr.replaceSelectionWith(node)
+                  view.dispatch(transaction)
+                }
+                if (file) reader.readAsDataURL(file)
+                return true
+              }
+            }
+
+            return false
+          },
+        },
+      }),
+    ]
+  },
+})
+
+export default ImagePaste
--- client/src/components/editor/extenstions/index.js
+++ client/src/components/editor/extenstions/index.js
@@ -1,8 +1,9 @@
-import StarterKit from './starterKit'
-import Image from './image'
-import TextStyle from './textStyle'
-import Color from './color'
+import StarterKit from '@/components/editor/extenstions/starterKit'
+import Image from '@/components/editor/extenstions/image'
+import TextStyle from '@/components/editor/extenstions/textStyle'
+import Color from '@/components/editor/extenstions/color'
+import ImagePaste from '@/components/editor/extenstions/imagePaste'
 
-export const extensions = [StarterKit, Image, TextStyle, Color]
+export const extensions = [StarterKit, Image, TextStyle, Color, ImagePaste]
 
 export default extensions
